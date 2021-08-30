3f8f0701e909b5cfeef713166a45aab982e3c047
xconfigurator <xconfigurator@hotmail.com>
2021-08-30 10:04:15 +0800

    1. 线程池框架和Future来控制线程执行时间。 2. try-catch-finally的用法。

---
+++ src/main/java/liuyang/test/HelloTryCatchFinally.java
@@ -0,0 +1,37 @@
+package liuyang.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author liuyang
+ * @date 2021/8/26
+ */
+@Slf4j
+public class HelloTryCatchFinally {
+    public static boolean testProcedureNormal() {
+        boolean flag = true;
+        try {
+            flag = true;
+            return flag;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            flag = false;
+            return flag;
+        } finally {
+            log.debug("Response = " + flag);
+        }
+    }
+
+    public static boolean testPeocedureAbnormal () {
+        boolean flag = true;
+        try {
+            throw new RuntimeException("故意的");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            flag = false;
+            return flag;
+        } finally {
+            log.debug("Response = " + flag);
+        }
+    }
+}
---
+++ src/test/java/liuyang/concurrency2021/future/ControlExecTimeTest.java
@@ -0,0 +1,35 @@
+package liuyang.concurrency2021.future;
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author liuyang
+ * @date 2021/8/27
+ */
+@Slf4j
+public class ControlExecTimeTest {
+
+    @BeforeEach
+    void init () {
+        ControlExecTime.EXEC_TIME = 1000l;
+        // ControlExecTime.TIME_OUT = 99l; // 10001l;
+        ControlExecTime.TIME_OUT = 10001l; // 10001l;
+    }
+
+    @Test
+    void test() {
+        //boolean b = ControlExecTime.timeControlMethod(ControlExecTime.IsAliveResponse.class);
+        //log.info("是否超时 = {}", b);
+        ControlExecTime.TimeControlReturnType<ControlExecTime.IsAliveResponse> isAliveResponseTimeControlReturnType
+                = ControlExecTime.timeControlMethod(ControlExecTime.IsAliveResponse.class);
+        log.info("response = {}", JSON.toJSONString(isAliveResponseTimeControlReturnType));
+        if (isAliveResponseTimeControlReturnType.isTimeout()) {
+            log.info("##超时！");
+        } else {
+            log.info("##未超时！");
+        }
+    }
+}
