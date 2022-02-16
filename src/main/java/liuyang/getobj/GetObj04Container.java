7b604f758cb2e02d0996d6517a99b5b985d74aa0
xconfigurator <xconfigurator@163.com>
2022-02-16 15:45:33 +0800

    回字的N种写法，我好像悟了！

---
+++ src/main/java/liuyang/getobj/GetObj04Container.java
@@ -0,0 +1,12 @@
+package liuyang.getobj;
+
+/**
+ * @author :liuyang(wx)
+ * @date :2022/2/16 15:10
+ */
+public class GetObj04Container {
+    public static void main(String[] args) {
+        // 1. 依赖查找 (JNDI)
+        // 2. 依赖注入（IoC/DI）
+    }
+}
---
+++ src/main/java/liuyang/getobj/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * "回有多少种写法"
+ * "极限的N种求法"
+ * ……
+ */
+package liuyang.getobj;
