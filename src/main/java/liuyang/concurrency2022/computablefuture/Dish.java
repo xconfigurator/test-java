63f2f1316a758ae774a9d7382bc9bafbd9cb30f3
liuyang <xconfigurator@hotmail.com>
2022-04-25 14:42:00 +0800

    CompletableFuture - 性能

---
+++ src/main/java/liuyang/concurrency2022/computablefuture/Dish.java
@@ -0,0 +1,35 @@
+package liuyang.concurrency2022.computablefuture;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Data
+@AllArgsConstructor
+@Slf4j
+@ToString
+public class Dish {
+    private String name;
+    private int productionTime;
+
+    public void make() {
+        try {
+            log.info("{}制作中...", this.toString());
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void make(int seconds) {
+        try {
+            log.info("{}制作中...", this.toString());
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
