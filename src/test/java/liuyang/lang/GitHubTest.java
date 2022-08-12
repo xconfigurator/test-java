f74bb1bf12e48de8a2ae1bb9fffa72f2cb9650ca
liuyang <xconfigurator@hotmail.com>
2022-08-12 10:07:57 +0800

    测试加密软件对本地提交影响

---
+++ src/test/java/liuyang/lang/GitHubTest.java
@@ -0,0 +1,16 @@
+package liuyang.lang;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author liuyang(wx)
+ * @since 2022/8/12
+ */
+@Slf4j
+public class GitHubTest {
+    @Test
+    void test() {
+        log.info("hei");
+    }
+}
