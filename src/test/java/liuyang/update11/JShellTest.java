8b8bd8a4527bb1c3e049293c4d61b1e68777d801
liuyang <xconfigurator@hotmail.com>
2023-11-03 21:33:44 +0800

    宋红康 课程笔记 JDK8+ 新特性

---
+++ src/test/java/liuyang/update11/JShellTest.java
@@ -0,0 +1,23 @@
+package liuyang.update11;
+
+/**
+ * JDK9+
+ *
+ * @author xconf
+ * @since 2023/11/3
+ */
+public class JShellTest {
+    // @since JDK 9
+    // REPL read-evaluate-print-loop
+    // https://www.bilibili.com/video/BV1PY411e7J6?p=198&vd_source=8bd7b24b38e3e12c558d839b352b32f4
+    // 从视频开始到 09:46
+    // /help
+    // /help intro      jshell工具的简介
+    // /list            列出当前session里所有有效的代码片段
+    // /vars            列出当前session下所有创建过的变量
+    // /methods         列出当前session下所有创建过的方法
+    // /imports         列处导入的包
+    // /history         键入内容的历史记录
+    // /edit            使用外部代码编辑器来编写Java代码
+    // /exit            退出
+}
