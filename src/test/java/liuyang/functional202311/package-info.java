0b2e725091acbf82d515f8be75bb6e1d92d5b6ce
liuyang <xconfigurator@hotmail.com>
2023-11-07 23:21:47 +0800

    康师傅的函数式编程讲座笔记完成

--- src/test/java/liuyang/functional202311/package-info.java
+++ src/test/java/liuyang/functional202311/package-info.java
@@ -3,9 +3,9 @@
  *
  * Lambda表达式 20231102
  * https://www.bilibili.com/video/BV1PY411e7J6/?p=195&spm_id_from=pageDriver&vd_source=8bd7b24b38e3e12c558d839b352b32f4
- * 方法引用与构造器引用 20231102 TODO
+ * 方法引用与构造器引用 20231102
  * https://www.bilibili.com/video/BV1PY411e7J6/?p=196&vd_source=8bd7b24b38e3e12c558d839b352b32f4
- * Stream API
+ * Stream API 20231107
  * https://www.bilibili.com/video/BV1PY411e7J6/?p=197&vd_source=8bd7b24b38e3e12c558d839b352b32f4
  *
  * @author xconf
