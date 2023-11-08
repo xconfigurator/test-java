f0eec9d3e29a40a2e87290ba42a59ad9930f1ce3
liuyang <xconfigurator@hotmail.com>
2023-11-09 05:48:02 +0800

    Guava 示例

---
+++ src/test/java/liuyang/tools/guava/collect/BiMapTest.java
@@ -0,0 +1,17 @@
+package liuyang.tools.guava.collect;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author xconf
+ * @since 2023/11/9
+ */
+public class BiMapTest {
+    @Test
+    void test() {
+        //BiMap
+        //HashBiMap
+    }
+}
