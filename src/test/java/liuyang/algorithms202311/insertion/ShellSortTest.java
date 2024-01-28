2237d0233b1cbdc95cac57abfb8efb7ed00e3c3c
liuyang <xconfigurator@hotmail.com>
2024-01-28 08:32:58 +0800

    shell排序

--- src/test/java/liuyang/algorithms202311/insertion/ShellSortTest.java
+++ src/test/java/liuyang/algorithms202311/insertion/ShellSortTest.java
@@ -1,8 +1,28 @@
 package liuyang.algorithms202311.insertion;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
 /**
+ * 实际应用中，如果待排序集合规模在几万的情况，配合Sedgewick增量序列效果比较好。
+ *
+ * 一个算法可以非常简单，但其时间复杂度分析会非常困难。
+ *
  * @author xconf
  * @since 2023/11/15
+ *        2023/11/23
  */
+@Slf4j
 public class ShellSortTest {
+
+    @Test
+    void test202311231538() {
+        // TODO
+
+    }
+
+    private static void shellSort(int[] array) {
+        // TODO
+
+    }
 }
